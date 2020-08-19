5bf2094f7d31dd80c442ab036860077e5a447026
armstrongch <christopherernestarmstrong@gmail.com>
2020-08-19 14:53:19 -0400

    Update dracula.js

--- dracula.js
+++ dracula.js
@@ -25,6 +25,47 @@ function draculaTurn()
 function dracula_flying()
 {
 	var messageText = "You can hear the beating of leathery wings as Dracula flies through the night sky.";
+	
+	// Search for the furthest location that Dracula hasn't searched
+	var currentTarget = {x:-1, y:-1};
+	var targetDistance = -1;
+	
+	for (let i = 0; i < map.length; i++)
+	{
+		for (let j = 0; j < map[i].length; j++)
+		{
+			if (!map[i][j].searchedByDracula)
+			{
+				testDistance = Math.abs(i - dracula.position.y) + Math.abs(j - dracula.position.x);
+				
+				if ((targetDistance == -1) || (testDistance > targetDistance) || ((testDistance == targetDistance) && (Math.random() > 0.5)))
+				{
+					currentTarget = {x: j, y: i};
+					targetDistance = testDistance;
+				}
+			}
+		}
+	}
+	
+	//If we found a sport, go there
+	if (targetDistance != -1)
+	{
+		dracula.position = currentTarget;
+	}
+	else
+	// otherwise, reset all locations and fly diagonally
+	{
+		for (let i = 0; i < map.length; i++)
+		{
+			for (let j = 0; j < map[i].length; j++)
+			{
+				map[i][j].searchedByDracula = false;
+			}
+		}
+		dracula.position.x = 4 - dracula.position.x;
+		dracula.position.y = 4 - dracula.position.y;
+	}
+	dracula.currentMode = "searching";
 	return messageText;
 }
 
